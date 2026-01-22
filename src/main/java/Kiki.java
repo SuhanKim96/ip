99f26ca3759ec88774125e142ba9b77fbd1db288
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2026-01-22 12:46:53 +0800

    Add Echo feature

--- src/main/java/Kiki.java
+++ src/main/java/Kiki.java
@@ -1,21 +1,39 @@
+import java.util.Scanner;
+
 /**
  * The main class of the Kiki application.
  */
 public class Kiki {
     /**
-     * Greets the user and exits the application.
+     * Greets the user, echoes commands, and exits when "bye" is entered.
      *
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
         String name = "Kiki";
         String horizontalLine = "____________________________________________________________";
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        String indent = "    ";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        System.out.println(indent + horizontalLine);
+        System.out.println(indent + "Hello! I'm " + name);
+        System.out.println(indent + "What can I do for you?");
+        System.out.println(indent + horizontalLine);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(indent + horizontalLine);
+                System.out.println(indent + "Bye. Hope to see you again soon!");
+                System.out.println(indent + horizontalLine);
+                break;
+            }
+
+            System.out.println(indent + horizontalLine);
+            System.out.println(indent + input);
+            System.out.println(indent + horizontalLine);
+        }
     }
+
 }
