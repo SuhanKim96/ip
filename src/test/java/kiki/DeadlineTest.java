bfe4d6b92670d25fcf815b590b1b3df783bff876
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2026-01-30 14:48:24 +0800

    Add JUnit tests

---
+++ src/test/java/kiki/DeadlineTest.java
@@ -0,0 +1,12 @@
+package kiki;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DeadlineTest {
+    @Test
+    public void testDeadlineString() throws KikiException {
+        Deadline d = new Deadline("return book", "2025-12-25");
+        assertEquals("[D][ ] return book (by: Dec 25 2025)", d.toString());
+    }
+}
